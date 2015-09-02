a5527a068d09ae0eb593f41cc91be27f833819c0
Philip Choi <grjkz0@gmail.com>
2015-09-02 17:05:08 -0400

    styled loading image

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,19 +1,9 @@
-// counter = 0;
-
-// function nextDiv() {
-// 	counter++;
-// 	$("#box"+counter).slideDown();
-// }
-
 $(document).ready(function() {
 	$('.submit_button').click(function() {
 		$('.loading-content').append("<img src='http://i.kinja-img.com/gawker-media/image/upload/ex9sgvlnuxlxicocpri4.gif'>")
 	})
-	// $('body').css('background-color','yellow');
-
 	$('.save_button').click(function(e) {
 		e.preventDefault();
-
 		$.ajax({
 			type: 'post',
 			url: '/saveadventure',
@@ -22,7 +12,7 @@ $(document).ready(function() {
 					$('.message').html("<h4>Saved!</h4>")
 				}
 				else if (status === 2) {
-					$('.message').html("<h4>You already saved it! Stop clicking it!</h4>")	
+					$('.message').html("<h4>You already saved it! Stop clicking it!</h4>")
 				}
 				else {
 					$('.message').html("<h4><a href='/login'>Log in to save!</a></h4>")
@@ -33,10 +23,4 @@ $(document).ready(function() {
 			}
 		})
 	})
-
-
-
-
-
-
 })
