6b5c47c9a97206acf2b328b8c434782bf804ad52
Philip Welber <prwelber@gmail.com>
2015-09-02 14:14:38 -0400

    deploy fixes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :users do 
     resources :adventures, only: [:index, :show]
   end
-
+  post '/saveadventure' => 'adventures#saveadventure'
   get "*path" => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
